84270bcb60beff398b3f8eac96826d81a573672b
Tom Robbins <tomrobbins@gmx.com>
2017-05-12 11:02:23 +0000

    Mixing up my scalars

--- sources/supervisor/owlevel/src/ucd.cc
+++ sources/supervisor/owlevel/src/ucd.cc
@@ -4,15 +4,15 @@
 
 namespace ll::Unicode {
     
-    ucd_name_t const* ucd_names = {
+    ucd_name_t const ucd_names[] = {
 #include "./ucd/name.g.inl"
     };
     
-    ucd_category_t const* ucd_categories = {
+    ucd_category_t const ucd_categories[] = {
 #include "./ucd/category.g.inl"
     };
     
-    ucd_block_t const* ucd_blocks = {
+    ucd_block_t const ucd_blocks[] = {
 #include "./ucd/block.g.inl"  
     };
     
